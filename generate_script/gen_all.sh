a89d96287254ffefd6433ac978bf7eb1d6a67517
RongxuCui <272190467@qq.com>
2026-03-19 02:54:57 +0000

    Stop tracking unused files

--- generate_script/gen_all.sh
+++
@@ -1,4 +0,0 @@
-python generate_script/gen_bddl.py --train --libero_task libero_10
-python generate_script/gen_bddl.py --train --libero_task libero_goal
-python generate_script/gen_bddl.py --train --libero_task libero_spatial
-python generate_script/gen_bddl.py --train --libero_task libero_object
